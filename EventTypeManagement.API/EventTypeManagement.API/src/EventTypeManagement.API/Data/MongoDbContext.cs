86749c6b1aac623b8f16671682c298e04328720d
RenJieJiang <jiangrj1@hotmail.com>
2024-12-20 00:05:34 +1100

    init project

---
+++ EventTypeManagement.API/EventTypeManagement.API/src/EventTypeManagement.API/Data/MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EventTypeManagement.API.Models;
+using MongoDB.Driver;
+
+namespace EventTypeManagement.API.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+            _database = client.GetDatabase("EventTypeDb");
+        }
+
+        public IMongoCollection<EventType> EventTypes => _database.GetCollection<EventType>("EventTypes");
+    }
+}
---
+++ EventTypeManagement.API/EventTypeManagement.API/src/EventTypeManagement.API/Models/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace EventTypeManagement.API.Models
+{
+    public class EventType
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

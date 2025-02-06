c2108c58888f57e43e90f4f952e1648eef0c8b06
RenJieJiang <jiangrj1@hotmail.com>
2025-02-07 00:18:41 +1100

    refactor bit code

--- eventflex-web/src/lib/schemas/auth.ts
+++ eventflex-web/src/lib/schemas/auth.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email({ message: "Invalid email address" }).trim(),
-    password: z.string().min(8, { message: "Password must be at least 8 characters" }).trim(),
+  email: z.string().trim().email({
+    message: "Invalid email address",
+  }),
+  password: z.string().trim().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
 });
